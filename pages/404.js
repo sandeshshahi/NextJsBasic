c4c5ba137f43fa970b60d948462966c4bebe61fe
sandesh shahi <sndshshahi@gmail.com>
2022-03-09 17:24:19 +0545

    404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h1>404 Page with all the custom styling necesssary</h1>
+        </div>
+    )
+}
+
+export default PageNotFound;
